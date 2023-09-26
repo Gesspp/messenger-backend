ad014e024f57781762006c28ff9f3009dc93d12c
Gesspp <nikitatum199@gmail.com>
2023-09-26 19:34:57 +0300

    регистрация

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,23 @@
-const signup = (req, res) => {
-    console.log(req.body);
+const bcrypt = require("bcrypt")
+const User = require("../models/user");
+
+const signup = async (req, res) => {
+    if (!req.body) return res.status(401).json({error: "no body"})
+
+    const { nickname, email, password } = req.body;
+    const existingNickname = await User.findOne({nickname});
+    const existingEmail = await User.findOne({email});
+
+    if (existingEmail || existingNickname) {
+        return res.status(400).json({error: "same nickname or email is existing"})
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({nickname, email, password: hashedPassword});
+    await user.save();
+
+    return res.status(201).json({message: "user successfully created"})
+
 }
 
 module.exports = {
