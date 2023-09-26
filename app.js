9a46ef3d564893333b9886d72e9f4544d7a00259
Gesspp <nikitatum199@gmail.com>
2023-09-26 18:58:36 +0300

    изменил порт

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use((req, res) => {
 async function main() {
     try{
         await mongoose.connect("mongodb://127.0.0.1:27017/messenger");
-        app.listen(3000);
+        app.listen(3002);
         console.log("Сервер подключен")
     } catch(err) {
         return console.log(err);
