e19db9233192f13f61847d3fa041798eaaa90d90
Gesspp <nikitatum199@gmail.com>
2024-01-23 19:31:57 +0300

    Ошибки

--- routes/chat.js
+++ routes/chat.js
@@ -1,10 +1,9 @@
 const { Router } = require("express");
 const { add, all } = require("../controllers/chat");
-const chat = require("../models/chat");
 
 const chatRouter = Router();
 
 chatRouter.post("/add", add);
 chatRouter.get("/", all);
 
-module.exports = chatRouter;к
+module.exports = chatRouter;
